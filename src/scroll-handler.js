0b3b5c4d5306536e953503fe2565b34f41b3680f
Martin Gustafsson <gusten86@gmail.com>
2015-05-09 01:02:54 +0200

    feat(virtual-repeat): initial commit

---
+++ src/scroll-handler.js
@@ -0,0 +1,171 @@
+export class ScrollHandler{
+  constructor(){
+    this.listener = null;
+    this.view = null;
+    this.indicator = null;
+    this.relative = null;
+    this.offset = null;
+    this.reference = null;
+    this.passed = null;
+    this.velocity = null;
+    this.frame = null;
+    this.timestamp = null;
+    this.ticker = null;
+    this.amplitude = null;
+    this.target = null;
+    this.timeConstant = 325;
+    this.firefoxMultitude = 15;
+    this.mouseMultitude = 1;
+    this.keyStep = 120;
+    this.isFirefox = navigator.userAgent.indexOf('Firefox') > -1;
+    this.hasTouchWin = navigator.msMaxTouchPoints && navigator.msMaxTouchPoints > 1;
+    this.hasPointer = !!window.navigator.msPointerEnabled;
+    this.hasKeyDown = 'onkeydown' in document;
+    this.hasWheelEvent = 'onwheel' in document;
+    this.hasMouseWheelEvent = 'onmousewheel' in document;
+  }
+
+  initialize(view, listener){
+    this.listener = listener;
+
+    if(this.hasWheelEvent){
+      view.addEventListener("wheel", e => this.wheel(e));
+    }
+
+    if(this.hasMouseWheelEvent){
+      view.addEventListener("mousewheel", e => this.mouseWheel(e));
+    }
+
+    if (typeof window.ontouchstart !== 'undefined') {
+      view.addEventListener('touchstart', e => this.touchStart(e));
+      view.addEventListener('touchmove', e => this.touchMove(e));
+      view.addEventListener('touchend', e => this.touchEnd(e));
+    }
+
+    if(this.hasPointer && this.hasTouchWin) {
+      this.bodyTouchAction = document.body.style.msTouchAction;
+      view.body.style.msTouchAction = "none";
+      view.addEventListener("MSPointerDown", e => this.tap(e), true);
+      view.addEventListener("MSPointerMove", e => this.drag(e), true);
+    }
+
+    if(this.hasKeyDown){
+      view.addEventListener("keydown", e => this.keyDown(e), false);
+    }
+
+    this.offset = 0;
+    this.pressed = false;
+  }
+
+  ypos(event){
+    if (event.targetTouches && (event.targetTouches.length >= 1)) {
+      return event.targetTouches[0].clientY;
+    }
+
+    return event.clientY;
+  }
+
+  track() {
+    var now, elapsed, delta, v;
+
+    now = Date.now();
+    elapsed = now - this.timestamp;
+    this.timestamp = now;
+    delta = this.offset - this.frame;
+    this.frame = this.offset;
+
+    v = 1000 * delta / (1 + elapsed);
+    this.velocity = 0.8 * v + 0.2 * this.velocity;
+  }
+
+  autoScroll() {
+    var elapsed, delta;
+
+    if (this.amplitude) {
+      elapsed = Date.now() - this.timestamp;
+      delta = this.amplitude * Math.exp(-elapsed / this.timeConstant);
+      if (delta > 0.5 || delta < -0.5) {
+        this.offset = this.listener(delta);
+        requestAnimationFrame(() => this.autoScroll());
+      }
+    }
+  }
+
+  touchMove(event) {
+    var y, delta;
+    if (this.pressed) {
+      y = this.ypos(event);
+      delta = this.reference - y;
+      if (delta > 2 || delta < -2) {
+        this.reference = y;
+        this.offset = this.listener(-delta);
+      }
+    }
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  }
+
+  touchStart(event) {
+    this.pressed = true;
+    this.reference = this.ypos(event);
+
+    this.velocity = this.amplitude = 0;
+    this.frame = this.offset;
+    this.timestamp = Date.now();
+    clearInterval(this.ticker);
+    this.ticker = setInterval(() => this.track(), 100);
+
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  }
+
+  touchEnd(event) {
+    this.pressed = false;
+
+    clearInterval(this.ticker);
+    if (this.velocity > 10 || this.velocity < -10) {
+      this.amplitude = 0.1 * this.velocity;
+      this.target = Math.round(this.offset + this.amplitude);
+      this.timestamp = Date.now();
+      requestAnimationFrame(() => this.autoScroll());
+    }
+
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  }
+
+  mouseWheel(event) {
+    var delta = (event.wheelDeltaY) ? event.wheelDeltaY : event.wheelDelta;
+
+    this.offset = this.listener(delta);
+  }
+
+  wheel(event) {
+    var delta = event.wheelDeltaY || event.deltaY;
+
+    if(this.isFirefox && event.deltaMode == 1) {
+      delta *= this.firefoxMultitude;
+    }
+
+    delta *= this.mouseMultitude;
+
+    this.offset = this.listener(delta);
+  }
+
+  keyDown(event) {
+    var delta = 0;
+    switch(event.keyCode) {
+      case 38:
+        delta = this.keyStep;
+        break;
+      case 40:
+        delta = -this.keyStep;
+        break;
+    }
+
+    this.offset = this.listener(delta);
+  }
+}
---
+++ src/virtual-repeat.js
@@ -0,0 +1,266 @@
+import {inject} from 'aurelia-dependency-injection';
+import {ObserverLocator, calcSplices, getChangeRecords} from 'aurelia-binding';
+import {BoundViewFactory, ViewSlot, customAttribute, bindable, templateController} from 'aurelia-templating';
+import {ScrollHandler} from './scroll-handler';
+
+@customAttribute('virtual-repeat')
+@bindable('items')
+@bindable('local')
+@templateController
+@inject(Element, BoundViewFactory, ViewSlot, ObserverLocator, ScrollHandler)
+export class VirtualRepeat {
+  constructor(element, viewFactory, viewSlot, observerLocator, scrollHandler){
+    this.element = element;
+    this.viewFactory = viewFactory;
+    this.viewSlot = viewSlot;
+    this.observerLocator = observerLocator;
+    this.scrollHandler = scrollHandler;
+    this.local = 'item';
+    this.ease = 0.1;
+    this.targetY = 0;
+    this.currentY = 0;
+    this.previousY = 0;
+    this.first = 0;
+    this.previousFirst = 0;
+    this.numberOfDomElements = 0;
+  }
+
+  bind(executionContext){
+    this.executionContext = executionContext;
+    this.virtualScroll = this.element.parentElement.parentElement;
+    this.virtualScroll.style.overflow = 'hidden';
+    this.virtualScroll.tabIndex = '999';
+    this.virtualScrollInner = this.element.parentElement;
+    this.virtualScroll.addEventListener('touchmove', function(e) { e.preventDefault(); });
+
+    this.scrollHandler.initialize(this.virtualScroll,  deltaY => {
+      this.targetY += deltaY;
+      this.targetY = Math.max(-this.scrollViewHeight, this.targetY);
+      this.targetY = Math.min(0, this.targetY);
+      return this.targetY;
+    });
+
+    window.onresize = () => {
+      this.handleContainerResize();
+    };
+
+    // create first item to get the heights
+    var row = this.createFullExecutionContext(this.items[0], 0, 1);
+    var view = this.viewFactory.create(row);
+    this.viewSlot.add(view);
+  }
+
+  unbind(){
+    this.scrollHandler.dispose();
+  }
+
+  attached(){
+    var row, view;
+    this.listItems = this.virtualScrollInner.children;
+    this.itemHeight = VirtualRepeat.calcOuterHeight(this.listItems[0]);
+
+    this.virtualScrollHeight = VirtualRepeat.calcScrollHeight(this.virtualScroll);
+    this.numberOfDomElements = Math.ceil(this.virtualScrollHeight / this.itemHeight) + 1;
+
+    for(var i = 1, ii = this.numberOfDomElements; i < ii; ++i){
+      row = this.createFullExecutionContext(this.items[i], i, ii);
+      view = this.viewFactory.create(row);
+      this.viewSlot.add(view);
+    }
+
+    this.calcScrollViewHeight();
+    this.processItems();
+  }
+
+  handleContainerResize(){
+    var children = this.viewSlot.children,
+        childrenLength = children.length,
+        row, view, addIndex;
+
+    this.virtualScrollHeight = VirtualRepeat.calcScrollHeight(this.virtualScroll);
+    this.numberOfDomElements = Math.ceil(this.virtualScrollHeight / this.itemHeight) + 1;
+
+    if(this.numberOfDomElements > childrenLength){
+      addIndex = children[childrenLength - 1].executionContext.$index + 1;
+      row = this.createFullExecutionContext(this.items[addIndex], addIndex, this.items.length);
+      view = this.viewFactory.create(row);
+      this.viewSlot.insert(childrenLength, view);
+    }else if (this.numberOfDomElements < childrenLength){
+      this.numberOfDomElements = childrenLength;
+    }
+
+    this.calcScrollViewHeight();
+  }
+
+  static calcOuterHeight(element){
+    var height, style, marginTop, marginBottom;
+    height = element.getBoundingClientRect().height;
+    style = element.currentStyle || window.getComputedStyle(element);
+    marginTop = parseInt(style.marginTop);
+    marginBottom = parseInt(style.marginBottom);
+    height += Number.isNaN(marginTop) ? 0 : marginTop;
+    height += Number.isNaN(marginBottom) ? 0 : marginBottom;
+
+    return height;
+  }
+
+  static calcScrollHeight(element){
+    var height, style, borderBottomWidth, borderTopWidth;
+    height = element.getBoundingClientRect().height;
+    style = element.currentStyle || window.getComputedStyle(element);
+    borderTopWidth = parseInt(style.borderTopWidth);
+    borderBottomWidth = parseInt(style.borderBottomWidth);
+    height -= Number.isNaN(borderTopWidth) ? 0 : borderTopWidth;
+    height -= Number.isNaN(borderBottomWidth) ? 0 : borderBottomWidth;
+    return height;
+  }
+
+  processItems(){
+    var items = this.items, observer;
+
+    observer = this.observerLocator.getArrayObserver(items);
+
+    this.disposeSubscription = observer.subscribe(splices => {
+      this.handleSplices(items, splices);
+    });
+
+    this.scroll();
+  }
+
+  scroll() {
+    var scrollView = this.virtualScrollInner,
+      itemHeight = this.itemHeight,
+      items = this.items,
+      viewSlot = this.viewSlot,
+      numberOfDomElements =  this.numberOfDomElements,
+      node, marginTop, translateStyle, view, first;
+
+    this.currentY += (this.targetY - this.currentY) * this.ease;
+    this.currentY = Math.round(this.currentY * 1);
+
+    if(this.currentY === this.previousY){
+      requestAnimationFrame(() => this.scroll());
+      return;
+    }
+
+    this.previousY = this.currentY;
+    this.first = Math.ceil(this.currentY / itemHeight) * -1;
+    first = this.first;
+
+    if(first > this.previousFirst && first + numberOfDomElements - 1 <= items.length){
+      this.previousFirst = first;
+
+      view = viewSlot.children[0];
+      console.log('Update at: ', first + numberOfDomElements - 1);
+      view.executionContext = this.updateExecutionContext(view.executionContext, first + numberOfDomElements - 1, items.length);
+      view.executionContext[this.local] = items[first + numberOfDomElements - 1];
+      viewSlot.children.push(viewSlot.children.shift());
+
+      // TODO move view container comments
+      node = scrollView.children[0];
+      scrollView.insertBefore(node, scrollView.children[numberOfDomElements]);
+
+      marginTop = itemHeight * first + "px";
+      scrollView.style.marginTop = marginTop;
+
+    }else if (first < this.previousFirst){
+      this.previousFirst = first;
+
+      view = viewSlot.children[numberOfDomElements - 1];
+      view.executionContext[this.local] = items[first];
+      view.executionContext = this.updateExecutionContext(view.executionContext, first, items.length);
+      viewSlot.children.unshift(viewSlot.children.splice(-1,1)[0]);
+
+      node = scrollView.children[numberOfDomElements - 1];
+      scrollView.insertBefore(node, scrollView.children[0]);
+
+      marginTop = itemHeight * first + "px";
+      scrollView.style.marginTop = marginTop;
+    }
+
+    translateStyle = "translate3d(0px," + this.currentY + "px,0px)";
+
+    this.virtualScrollInner.style.webkitTransform = translateStyle;
+    this.virtualScrollInner.style.msTransform = translateStyle;
+    this.virtualScrollInner.style.transform = translateStyle;
+
+    requestAnimationFrame(() => this.scroll());
+  }
+
+  createBaseExecutionContext(data){
+    var context = {};
+    context[this.local] = data;
+    return context;
+  }
+
+  createFullExecutionContext(data, index, length){
+    var context = this.createBaseExecutionContext(data);
+    return this.updateExecutionContext(context, index, length);
+  }
+
+  updateExecutionContext(context, index, length){
+    var first = (index === 0),
+      last = (index === length - 1),
+      even = index % 2 === 0;
+
+    context.$parent = this.executionContext;
+    context.$index = index;
+    context.$first = first;
+    context.$last = last;
+    context.$middle = !(first || last);
+    context.$odd = !even;
+    context.$even = even;
+
+    return context;
+  }
+
+  updateItems(){
+    var viewSlot = this.viewSlot,
+        items = this.items,
+        i, ii, view;
+
+    for(i = 0, ii = viewSlot.children.length; i < ii; ++i){
+      view = viewSlot.children[i];
+      view.executionContext[this.local] = items[this.first + i];
+      view.executionContext = this.updateExecutionContext(view.executionContext, this.first + i, items.length);
+    }
+  }
+
+  handleSplices(items, splices){
+    var numberOfDomElements = this.numberOfDomElements,
+      viewSlot = this.viewSlot,
+      first = this.first,
+      totalAdded = 0,
+      i, ii, j, view, marginTop, addIndex, splice, end, atBottom;
+    this.items = items;
+
+    this.updateItems();
+
+    for(i = 0, ii = splices.length; i < ii; ++i){
+      splice = splices[0];
+      addIndex = splices[i].index;
+      end = splice.index + splice.addedCount;
+      totalAdded += splice.addedCount;
+
+      for (; addIndex < end; ++addIndex) {
+        if(addIndex < first + numberOfDomElements && !atBottom){
+          marginTop = this.itemHeight * first + "px";
+          this.virtualScrollInner.style.marginTop = marginTop;
+        }
+      }
+    }
+
+    if(items.length < numberOfDomElements){
+      var limit = numberOfDomElements - (numberOfDomElements - items.length) - 1;
+      for(j = 0; j < numberOfDomElements; ++j){
+        this.virtualScrollInner.children[j].style.display = j >= limit ? 'none' : 'block';
+      }
+    }
+
+    this.calcScrollViewHeight();
+  }
+
+  calcScrollViewHeight(){
+    this.scrollViewHeight = (this.items.length * this.itemHeight) - this.virtualScrollHeight;
+  }
+}
